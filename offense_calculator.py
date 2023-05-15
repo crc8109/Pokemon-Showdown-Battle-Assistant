4f669d7d939e228eb2b53a5778f8b4d0ce734475
Bruno Diaz <56422761+crc8109@users.noreply.github.com>
2023-05-14 20:25:43 -0700

    Update schema

--- offense_calculator.py
+++ offense_calculator.py
@@ -1,371 +1,152 @@
 def offense_calculator(opponent_type1, opponent_type2):
     """This function will calculate the offensive multiplier for opponent Pokemon's type(s)"""
 
+    opponent_types = [opponent_type1, opponent_type2]
+
     # Dictionary for the offensive multipliers for each of the types below:
     offense_multiplier_dict = {
         "normal": {
-            "normal": 1,
-            "fire": 1,
-            "water": 1,
-            "electric": 1,
-            "grass": 1,
-            "ice": 1,
-            "fighting": 1,
-            "poison": 1,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 0,
-            "dragon": 1,
-            "dark": 1,
-            "steel": 1,
-            "fairy": 1
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "dragon", "dark", "steel", "fairy", "none"],
+            0: ["ghost"]
         },
         "fire": {
-            "normal": 1,
-            "fire": 0.5,
-            "water": 0.5,
-            "electric": 1,
-            "grass": 2,
-            "ice": 2,
-            "fighting": 1,
-            "poison": 1,    
-            "ground": 1,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 2,
-            "rock": 0.5,
-            "ghost": 1,
-            "dragon": 0.5,
-            "dark": 1,
-            "steel": 2,
-            "fairy": 1
+            2: ["grass", "ice", "bug", "steel"],
+            1: ["normal", "electric", "fighting", "poison", "ground", "flying", "psychic", "ghost", "dark", "fairy", "none"],
+            0.5: ["fire", "water", "rock", "dragon"]
         },
         "water": {
-            "normal": 1,
-            "fire": 2,
-            "water": 0.5,
-            "electric": 1,
-            "grass": 0.5, 
-            "ice": 1, 
-            "fighting": 1,
-            "poison": 1,
-            "ground": 2,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 2,
-            "ghost": 1,
-            "dragon": 0.5,
-            "dark": 1,
-            "steel": 1,
-            "fairy": 1
+            2: ["fire", "ground", "rock"],
+            1: ["normal", "electric", "ice", "fighting", "poison", "flying", "psychic", "bug", "ghost", "dark", "steel", "fairy", "none"],
+            0.5: ["water", "grass", "dragon"]
         },
         "electric": {
-            "normal": 1,
-            "fire": 1,
-            "water": 2,
-            "electric": 0.5,
-            "grass": 0.5,
-            "ice": 1,
-            "fighting": 1,
-            "poison": 1,
-            "ground": 0,
-            "flying": 2,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 1,
-            "dragon": 0.5,
-            "dark": 1,
-            "steel": 1,
-            "fairy": 1
+            2: ["water", "flying"],
+            1: ["normal", "fire", "ice", "fighting", "poison", "psychic", "bug", "rock", "dark", "steel", "fairy", "none"],
+            0.5: ["electric", "grass", "dragon", "ground"],
+            0: ["ground"]
         },
         "grass": {
-            "normal": 1,
-            "fire": 0.5,
-            "water": 2,
-            "electric": 1,
-            "grass": 0.5,
-            "ice": 1,
-            "fighting": 1,
-            "poison": 0.5,
-            "ground": 2,
-            "flying": 0.5,
-            "psychic": 1,
-            "bug": 0.5,
-            "rock": 2,
-            "ghost": 1,
-            "dragon": 0.5,
-            "dark": 1,
-            "steel": 0.5,
-            "fairy": 1
+            2: ["water", "ground", "rock"],
+            1: ["normal", "electric", "ice", "fighting", "psychic", "ghost", "dark", "fairy", "none"],
+            0.5: ["fire", "grass", "poison", "flying", "bug", "dragon", "steel"]
         },
         "ice": {
-            "normal": 1,
-            "fire": 0.5,
-            "water": 0.5,
-            "electric": 1,
-            "grass": 2,
-            "ice": 0.5,
-            "fighting": 1,
-            "poison": 1,
-            "ground": 2,
-            "flying": 2,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 1,
-            "dragon": 2,
-            "dark": 1,
-            "steel": 0.5,
-            "fairy": 1
+            2: ["grass", "ground", "flying", "dragon"],
+            1: ["normal", "electric", "fighting", "poison", "psychic", "bug", "rock", "ghost", "dark", "fairy", "none"],
+            0.5: ["fire", "water", "ice", "steel"]
         },
         "fighting": {
-            "normal": 2,
-            "fire": 1,
-            "water": 1,
-            "electric": 1,
-            "grass": 1,
-            "ice": 2,
-            "fighting": 1,
-            "poison": 0.5,
-            "ground": 1,
-            "flying": 0.5,
-            "psychic": 0.5,
-            "bug": 0.5,
-            "rock": 2,
-            "ghost": 0,
-            "dragon": 1,
-            "dark": 2,
-            "steel": 2,
-            "fairy": 0.5
+            2: ["normal", "ice", "rock", "dark", "steel"],
+            1: ["fire", "water", "electric", "grass", "fighting", "ground", "dragon", "none"],
+            0.5: ["poison", "flying", "psychic", "bug", "fairy"],
+            0: ["ghost"]
         },
         "poison": {
-            "normal": 1,
-            "fire": 1,
-            "water": 1,
-            "electric": 1,
-            "grass": 2,
-            "ice": 1,
-            "fighting": 1,
-            "poison": 0.5,
-            "ground": 0.5,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 0.5,
-            "ghost": 0.5,
-            "dragon": 1,
-            "dark": 1,
-            "steel": 0,
-            "fairy": 2
+            2: ["grass", "fairy"],
+            1: ["normal", "fire", "water", "electric", "ice", "fighting", "flying", "psychic", "bug", "dragon", "none"],
+            0.5: ["poison", "ground", "rock", "ghost"],
+            0: ["steel"]
         },
         "ground": {
-            "normal": 1,
-            "fire": 2,
-            "water": 1,
-            "electric": 2,
-            "grass": 0.5,
-            "ice": 1,
-            "fighting": 1,
-            "poison": 2,
-            "ground": 1,
-            "flying": 0,
-            "psychic": 1,
-            "bug": 0.5,
-            "rock": 2,
-            "ghost": 1,
-            "dragon": 1,
-            "dark": 1,
-            "steel": 2,
-            "fairy": 1
+            2: ["fire", "electric", "poison", "rock", "steel"],
+            1: ["normal", "water", "ice", "fighting", "ground", "psychic", "ghost", "dragon", "dark", "fairy", "none"],
+            0.5: ["grass", "bug"],
+            0: ["flying"]
         },
         "flying": {
-            "normal": 1,
-            "fire": 1,
-            "water": 1,
-            "electric": 0.5,
-            "grass": 2,
-            "ice": 1,
-            "fighting": 2,
-            "poison": 1,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 2,
-            "rock": 0.5,
-            "ghost": 1,
-            "dragon": 1,
-            "dark": 1,
-            "steel": 0.5,
-            "fairy": 1
+            2: ["grass", "fighting", "bug"],
+            1: ["normal", "fire", "water", "ice", "poison", "ground", "flying", "psychic", "ghost", "dragon", "dark", "fairy", "none"],
+            0.5: ["electric", "rock", "steel"],
+            0: ["ground"]
         },
         "psychic": {
-            "normal": 1,
-            "fire": 1,
-            "water": 1,
-            "electric": 1,
-            "grass": 1,
-            "ice": 1,
-            "fighting": 2,
-            "poison": 2,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 0.5,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 1,
-            "dragon": 1,
-            "dark": 0,
-            "steel": 0.5,
-            "fairy": 1
+            2: ["fighting", "poison"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "ground", "flying", "bug", "rock", "dragon", "fairy", "none"],
+            0.5: ["psychic", "steel"],
+            0: ["dark"]
         },
         "bug": {
-            "normal": 1,
-            "fire": 0.5,
-            "water": 1,
-            "electric": 1,
-            "grass": 2,
-            "ice": 1,
-            "fighting": 0.5,
-            "poison": 0.5,
-            "ground": 1,
-            "flying": 0.5,
-            "psychic": 2,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 0.5,
-            "dragon": 1,
-            "dark": 2,
-            "steel": 0.5,
-            "fairy": 0.5
+            2: ["grass", "psychic", "dark"],
+            1: ["normal", "water", "electric", "ice", "ground", "bug", "rock", "dragon", "none"],
+            0.5: ["fire", "fighting", "poison", "flying", "ghost", "steel", "fairy"]
         },
         "rock": {
-            "normal": 1,
-            "fire": 2,
-            "water": 1,
-            "electric": 1,
-            "grass": 1,
-            "ice": 2,
-            "fighting": 0.5,
-            "poison": 1,
-            "ground": 0.5,
-            "flying": 2,
-            "psychic": 1,
-            "bug": 2,
-            "rock": 1,
-            "ghost": 1,
-            "dragon": 1,
-            "dark": 1,
-            "steel": 0.5,
-            "fairy": 1
+            2: ["fire", "ice", "flying", "bug"],
+            1: ["normal", "water", "electric", "grass", "fighting", "psychic", "rock", "ghost", "dragon", "dark", "fairy", "none"],
+            0.5: ["fighting", "ground", "steel"]
         },
         "ghost": {
-            "normal": 0,
-            "fire": 1,
-            "water": 1,
-            "electric": 1,
-            "grass": 1,
-            "ice": 1,
-            "fighting": 1,
-            "poison": 1,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 2,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 2,
-            "dragon": 1,
-            "dark": 0.5,
-            "steel": 1,
-            "fairy": 1
+            2: ["psychic", "ghost"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "bug", "rock", "dragon", "steel", "fairy", "none"],
+            0.5: ["dark"],
+            0: ["normal"]
         },
         "dragon": {
-            "normal": 1,
-            "fire": 0.5,
-            "water": 0.5,
-            "electric": 0.5,
-            "grass": 0.5,
-            "ice": 2,
-            "fighting": 1,
-            "poison": 1,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 1,
-            "dragon": 2,
-            "dark": 1,
-            "steel": 1,
-            "fairy": 2
+            2: ["dragon"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "dark", "none"],
+            0.5: ["steel"],
+            0: ["fairy"]
         },
         "dark": {
-            "normal": 1,
-            "fire": 1,
-            "water": 1,
-            "electric": 1,
-            "grass": 1,
-            "ice": 1,
-            "fighting": 0.5,
-            "poison": 1,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 2,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 2,
-            "dragon": 1,
-            "dark": 0.5,
-            "steel": 1,
-            "fairy": 0.5
+            2: ["psychic", "ghost"],
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "poison", "ground", "flying", "bug", "rock", "dragon", "steel", "none"],
+            0.5: ["fighting", "dark", "fairy"]
         },
         "steel": {
-            "normal": 1,
-            "fire": 0.5,
-            "water": 0.5,
-            "electric": 0.5,
-            "grass": 1,
-            "ice": 2,
-            "fighting": 1,
-            "poison": 1,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 2,
-            "ghost": 1,
-            "dragon": 1,
-            "dark": 1,
-            "steel": 0.5,
-            "fairy": 2
+            2: ["ice", "rock", "fairy"],
+            1: ["normal", "grass", "fighting", "poison", "ground", "flying", "psychic", "bug", "ghost", "dragon", "dark", "none"],
+            0.5: ["fire", "water", "electric", "steel"]
         },
         "fairy": {
-            "normal": 1,
-            "fire": 0.5,
-            "water": 1,
-            "electric": 1,
-            "grass": 1,
-            "ice": 1,
-            "fighting": 2,
-            "poison": 0.5,
-            "ground": 1,
-            "flying": 1,
-            "psychic": 1,
-            "bug": 1,
-            "rock": 1,
-            "ghost": 1,
-            "dragon": 0,
-            "dark": 2,
-            "steel": 0.5,
-            "fairy": 1
+            2: ["fighting", "dragon", "dark"],
+            1: ["normal", "water", "electric", "grass", "ice", "ground", "flying", "psychic", "bug", "rock", "ghost", "fairy", "none"],
+            0.5: ["fire", "poison", "steel"]
+        },
+        "none": {
+            1: ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "dragon", "dark", "steel", "fairy"]
         }
     }
+
+    # This is the result of each type's offensive analysis (i.e its multiplier against each type)
+    # It's a dictionary who's keys are multipiers (e.g. 2, 0.5, 0) 
+    # and who's values are lists of types (e.g. ["grass", "ice", "bug", "steel"]
+    battle_analysis = {}
+
 
     
 
 
+    # This is the logic that will be used to fill out the temporary data structure for each type in opponent_types:
+    for type in opponent_types:
 
+        print(f"\nPrinting current type: {type}")
+
+        # create a variable called "multiplier" 
+        # it is just the current key in battle_analysis:
+        for type in range(0, len(offense_multiplier_dict))
+        # print current multiplier:
+        print(f"Printing current multiplier: {multiplier}")
+        
+
+        
+        # Look up the type in the dictionary
+        # variable called "multiplier" will equal each key in the dictionary
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+       
+        print(battle_analysis)
+
+        return
